10e4a0bae05818c7c47f44701949a7567b867060
Joss-gc <gcjosuealexis@gmail.com>
2026-02-13 00:07:34 -0600

    Termine la practica 6 Modales y Validaciones

---
+++ practica-js-modal/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Contacto } from './contacto/contacto'; // Ajusta la ruta si es necesario
+
+export const routes: Routes = [
+    { path: '', component: Contacto }
+];
